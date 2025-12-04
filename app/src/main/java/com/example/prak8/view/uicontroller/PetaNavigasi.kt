2ff684013acb6a0287844cb44347beb92ba9a479
Asyurazhr <Asyuradjola2004@gmail.com>
2025-12-04 09:11:44 +0700

    menambahkan host navigasi untuk composable

--- app/src/main/java/com/example/prak8/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/prak8/view/uicontroller/PetaNavigasi.kt
@@ -20,3 +20,9 @@ fun SiswaApp(navController: NavHostController= rememberNavController(), modifier
     HostNavigasi(navController = navController)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
