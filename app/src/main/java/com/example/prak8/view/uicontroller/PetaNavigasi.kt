8f59e4653a239ecc10d9f595c44967d545220aaf
Asyurazhr <Asyuradjola2004@gmail.com>
2025-12-04 09:10:22 +0700

    Add Composable untuk PetaNavigasu.kt

---
+++ app/src/main/java/com/example/prak8/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,18 @@
+package com.example.prak8.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
+import com.example.prak8.view.EntrySiswaScreen
+import com.example.prak8.view.HomeScreen
+import com.example.prak8.view.route.DestinasiEntry
+import com.example.prak8.view.route.DestinasiHome
+
+
+@Composable
