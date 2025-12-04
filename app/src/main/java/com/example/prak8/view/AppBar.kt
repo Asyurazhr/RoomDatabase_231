c2af0880fd80f8f0e07e0d8e45ae66852de1ebb8
Asyurazhr <Asyuradjola2004@gmail.com>
2025-12-04 09:16:15 +0700

    package com.example.prak8.view.uicontroller

---
+++ app/src/main/java/com/example/prak8/view/AppBar.kt
@@ -0,0 +1,21 @@
+package com.example.prak8.view.uicontroller
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    scrollBehavior: TopAppBarScrollBehavior,
+    onNavigateUp: (() -> Unit)? = null
+) {
