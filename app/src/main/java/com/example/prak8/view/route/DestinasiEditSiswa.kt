0d183d8d3cdab391923070f437ab5a6b29dc7ea6
Asyurazhr <Asyuradjola2004@gmail.com>
2025-12-04 16:04:41 +0700

    menambahkan titleRes edit_siswa beserta argumen idSiswa dan routeWithArgs

--- app/src/main/java/com/example/prak8/view/route/DestinasiEditSiswa.kt
+++ app/src/main/java/com/example/prak8/view/route/DestinasiEditSiswa.kt
@@ -4,3 +4,7 @@ import com.example.prak8.R
 
 object DestinasiEditSiswa : DestinasiNavigasi {
     override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
